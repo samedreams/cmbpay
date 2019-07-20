b232d4ae40fd0e70d8fba8ad5428f8aa164d2fd8
wujunze <itwujunze@163.com>
2019-07-20 12:05:57 +0800

    add client

---
+++ src/Client.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ *
+ * This file is part of the codemore/cmbpay package.
+ *
+ * (c) Codemore https://github.com/samedreams/cmbpay
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Codemore\Cmbpay;
+
+class Client
+{
+}
